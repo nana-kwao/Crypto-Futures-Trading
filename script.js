320723fc3cfd405a252c63015d4b8b2a37d743f7
Nana <99691633+nana-kwao@users.noreply.github.com>
2024-01-18 03:07:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-const principal = document.querySelector("#total").value;
-let total = principal;
-const numberOfTrades = document.querySelector("#number-trades").value; 
-const perTrade = document.querySelector("#per-trade").value;
-const perTakeProfit = document.querySelector("#per-take-profit").value;
-let total_remain = 1 - perTrade;
-let totalRemain;
-
 const submitBtn = document.querySelector("#submit-btn");
-const Calc = () =>{
-  for (let a = 1; a < (numberOfTrades +1); a++){
+
+const Calc = () => {
+  const principal = parseFloat(document.querySelector("#total").value);
+  let total = principal;
+  const numberOfTrades = parseInt(document.querySelector("#number-trades").value);
+  const perTrade = parseFloat(document.querySelector("#per-trade").value);
+  const perTakeProfit = parseFloat(document.querySelector("#per-take-profit").value);
+  let total_remain = 1 - perTrade;
+  let totalRemain;
+
+  for (let a = 1; a < (numberOfTrades + 1); a++) {
     let total_perTrade = (total * perTrade);
     totalRemain = (total * total_remain);
-    let recuring = total_perTrade * perTakeProfit;
-    total = recuring + total;   
+    let recurring = total_perTrade * perTakeProfit;
+    total = recurring + total;
   }
-  allTotal = total + totalRemain;
 
-  function displayResults(){
+  const allTotal = total + totalRemain;
+
+  function displayResults() {
     const resultDisplay = document.querySelector(".result-display");
     resultDisplay.innerHTML = `
       <h1>Results</h1>
       <div>
         <p>Principal Amount = ${principal}</p>
         <p>Profit on ${perTrade}% = ${total}</p>
-        <p>Profit on remaining ${total_remain}% = ${totalRemain}</p>
+        <p>Profit on remaining ${total_remain * 100}% = ${totalRemain}</p>
         <p>Total Profit: ${allTotal}</p>
       </div>
     `;
   }
 
-  return displayResults();
-}
-submitBtn.addEventListener("click", (e) =>{
-  e.preventDefault();
-  Calc()
-});
+  displayResults();
+};
+
+submitBtn.addEventListener("click",Calc);
